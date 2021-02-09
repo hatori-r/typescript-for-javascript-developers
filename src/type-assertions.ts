adc960ef9bb27d3ee9003cc08d95ddcaaa207658
hatori-r <aruue368@gmail.com>
2021-02-09 23:12:48 +0900

    commit

---
+++ src/type-assertions.ts
@@ -0,0 +1,9 @@
+export {};
+
+let name:any = 'Ham';
+
+// let length = name.length as number;
+// let length = (name as string).length; // より早い段階で型アサーションの定義を行うことができる。
+// let length = (<string>name).length; // 上記と同じ意味だがこの記法はJSXと似た構造になってしまうため非推奨となっている。
+
+// length = 'foo';
